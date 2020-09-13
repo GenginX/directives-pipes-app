aefe5f5dfa541f41a2539b4a151a8b400cd39ab9
kaczmarm <0kaczmar9@gmail.com>
2020-09-13 10:51:17 +0200

    Tworzenie dyrektyw i pipow + bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-pipes-app';
+  public LoremValue = 'LoremIpsum';
+  public zupa = 'EXAMPLE';
+  public JsonObject = {
+    properties: 'lorem'
+  };
+  public birthday = Date.now();
+  public isAuthenticated = false;
+  public people: string[] = ['Adam Kowalski', 'Marcin Janiak', 'Robert Lewandowski'];
+  public kebabCase = 'lore-ipsum-blablabla';
 }
